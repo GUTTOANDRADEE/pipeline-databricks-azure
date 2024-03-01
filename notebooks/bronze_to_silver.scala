4249ecccb045840c0765442bd48c59454c91da8a
GUTTOANDRADEE <GUTTOANDRADEE>
2024-03-01 23:19:54 +0000

    notebooks do pipeline

---
+++ notebooks/bronze_to_silver.scala
@@ -0,0 +1,65 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ## Conferindo se os dados contam na bronze
+
+// COMMAND ----------
+
+// MAGIC %py
+// MAGIC dbutils.fs.ls("/mnt/dados/bronze")
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Lendo dados na camada bronze
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/bronze/dataset_imoveis"
+val df = spark.read.format("delta").load(path)
+
+// COMMAND ----------
+
+display(df)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Transformando campos do json em colunas
+
+// COMMAND ----------
+
+display(df.select("anuncio.*"))
+
+// COMMAND ----------
+
+display(
+  df.select("anuncio.*","anuncio.endereco.*")
+  )
+
+// COMMAND ----------
+
+val dados_detalhados =   df.select("anuncio.*","anuncio.endereco.*")
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Removendo colunas
+
+// COMMAND ----------
+
+val df_silver = dados_detalhados.drop("caracteristicas","endereco")
+display(df_silver)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Salvando na camada silver
+
+// COMMAND ----------
+
+val path = "dbfs:/mnt/dados/silver/dataset_imoveis"
+df_silver.write.format("delta").mode("overwrite").save(path)
+
+// COMMAND ----------
+
+
